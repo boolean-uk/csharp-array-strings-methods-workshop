f15b4c639fdbaaac9a67eec1c2d56081f35363b4
uerbzr <nigel.sibbert@gmail.com>
2023-04-27 10:52:56 +0100

    Add project files.

---
+++ methodsstringsarrays.methodsscope/Program.cs
@@ -0,0 +1,47 @@
+﻿using methodsstringsarrays.methodsscope;
+
+//methods & scope
+
+int n = 3;
+decimal m = 3.99M;
+string s = "hi";
+Console.WriteLine("Hello, World!" + m.ToString() + n.ToString());
+
+
+
+//declare multiple lines
+Person p1 = new Person();
+p1.Name = "Nigel";
+p1.Age = 21;
+
+Console.WriteLine(p1.Age);
+
+//call method (no return type & no params)
+p1.InitializeMyAge();
+Console.WriteLine(p1.Age);
+
+
+p1.WakesUp();
+
+Console.WriteLine(p1.AgeNextYear());
+
+Console.WriteLine(p1.ageInHowManyYears(10));
+
+//declare in one hit
+Motorbike bike1 = new Motorbike()
+{
+    Name = "KTM 390",
+    CC = "373cc"
+};
+
+Person p2 = new Person()
+{
+    Name = "Libby"
+
+};
+
+p2.JustBoughtAMotorbike(bike1);
+
+p2.MyBike.StartMotorbike();
+Console.WriteLine(p2.MyBike.Name);
+Console.WriteLine(p2.WhatBikeHaveIJustBought());
---
+++ methodsstringsarrays.stringsarrays/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+Console.WriteLine("Enter a string");
+string result = Console.ReadLine();
+
+Console.WriteLine(result.ToUpper());
+Console.WriteLine(result.ToLower());
+Console.WriteLine(result.Trim());
+
+
+//this works!
+string name = "Nigel Sibbert";
+Console.WriteLine(name.Substring(name.IndexOf(' '), name.Length - name.IndexOf(' ')).Trim());
+
+//0123
+//OneTwoThree
+string substring = result.Substring(3, 3);
+Console.WriteLine(substring);
+
+
+//reverse
+char[] str = result.ToCharArray();
+Console.WriteLine(str.Reverse());
+Array.Reverse(str);
+string reverse = new string(str);
+Console.WriteLine(reverse);
+
+
+
+Console.WriteLine($"you entered: {result}");
+
+
+
+//more array
+string[] nameArray = { "Nigel", "Stitch", "Lola" };
+
+
+for(int i = 0; i < nameArray.Length; i++)
+{
+    Console.WriteLine(i);
+}
+
+foreach(string s in nameArray)
+{
+    Console.WriteLine(s);
+}
+
